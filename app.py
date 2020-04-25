6c1055d6aa9b3a6212f6693ab3a24b9c3696ff84
“[firstname <engyasmenamr@gmail.com>
2020-04-25 08:22:48 +0200

    uploading

---
+++ app.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  23 17:43:39 2020
+
+@author: yasmen amr
+"""
+import os
+import pathlib
+import statistics
+from collections import OrderedDict
+
+import pathlib as pl
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+from dash.dependencies import Input, Output, State
+
+import utils
+APP_PATH=os.path.dirname(os.path.abspath(__file__))
+
+
+book_info = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "items_info222.dat")),sep='\t')
+book_ratings = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "book_ratings.dat")),sep='\t',index_col=False)
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.config.suppress_callback_exceptions = True
+
+
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))],id='table')])
+
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+APP_PATH=os.path.dirname(os.path.abspath(__file__))
+app.layout = html.Div(children=[
+        html.Div(children=[
+
+            html.H2("Book Recommendation",style={'color':'white','textAlign': 'center', 'margin-bottom': 5 ,'background':'#00CCFF','text-height':'500px'}),
+            html.A(
+                id="gh-link",
+                children=["View on GitHub"],
+                href="https://github.com/plotly/dash-sample-apps/tree/master/apps/dash-pk-calc",
+                style={"color": "black", "border": "solid 1px white",'background':'#CCFFF'},
+
+
+                  )
+                           ]
+                ),
+        html.Div(children=[
+
+            html.H4(''),
+
+            dcc.Input(id='input-1-state', type='number', placeholder='enter your id'),
+            html.Button(id='submit-button-state', n_clicks =0, children='Submit'),
+            html.H4(children='The site will recommend books for you based on what you like'),
+
+            dash_table.DataTable(id='table1',
+                                columns=[{"name": i, "id": i} for i in utils.top_5_recommendations(-1).columns] ,
+                                style_data={"text-align":"left"},style_header={"text-align":"left"}
+
+
+                                )
+
+                            ]
+                )
+                                ]
+                    )
+
+
+@app.callback(Output('table1','data'),
+              [Input('submit-button-state', 'n_clicks')],
+              [State('input-1-state', 'value')])
+
+
+def update_output(x,n_clicks):
+    return   utils.top_5_recommendations(x).to_dict('records')[:5]
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  24 18:48:55 2020
+
+@author: yasmen amr
+"""
+import os
+import pandas as pd
+from surprise import Dataset, KNNWithMeans, Reader
+
+APP_PATH=os.path.dirname(os.path.abspath(__file__))
+
+book_info = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "items_info222.dat")),sep='\t',index_col=False)
+book_ratings = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "book_ratings.dat")),sep='\t',index_col=False)
+
+
+
+reader = Reader(rating_scale=(1, 10))
+data = Dataset.load_from_df(book_ratings[['user', "item", "rating"]], reader)
+
+# To use item-based cosine similarity
+sim_options = {
+    "name": "cosine",
+    "user_based": False,  # Compute  similarities between items
+}
+model = KNNWithMeans(sim_options=sim_options)
+
+trainingSet = data.build_full_trainset()
+model.fit(trainingSet)
+
+
+book_ratings2=book_ratings.drop(['user'], axis=1)
+book_ratings2=book_ratings2.groupby('item').mean()
+book_ratings2['Book_ID']=book_ratings2.index
+
+#join in ratio from the other table
+
+book_info['rating']= None
+book_info = book_info.join(book_ratings2.set_index(["Book_ID"])["rating"].to_frame("ratio2"), on=["Book_ID"])
+
+# take ratio2 first, then the existing ratio value if ratio2 is null
+book_info["rating"] = book_info["ratio2"].fillna(book_info["rating"])
+
+# delete the ratio2 column
+del book_info["ratio2"]
+book_info['average_rating']=book_info['rating']
+book_info=book_info.drop(['rating'],axis=1)
+
+def top_5_recommendations(uid):
+    books_rated_by_user = book_ratings.loc[book_ratings['user'] == uid, "item"].values
+
+    other_books = book_info.loc[~book_info['Book_ID'].isin(books_rated_by_user)].sort_values(
+        by='average_rating', ascending=False)
+
+    # if the user exists or not
+    if uid not in set(book_ratings['user']):
+        return other_books[:5]
+
+    other_books['user_rate'] = other_books['Book_ID'].apply(lambda x: model.predict(uid, x).est)
+
+    return other_books.sort_values(by='user_rate', ascending=False).drop(['user_rate'],axis=1)
